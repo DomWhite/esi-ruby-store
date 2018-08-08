6381993706b04a290f91d8ad6ca917bba6ef819e
Dominic White <dominic.white@reedonline.co.uk>
2018-08-08 10:33:37 +0100

    changed IP again

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server '34.245.69.228', user: 'deploy', roles: %w{web app db}
+server '34.253.125.240', user: 'deploy', roles: %w{web app db}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
